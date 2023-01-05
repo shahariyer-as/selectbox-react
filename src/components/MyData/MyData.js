beb73a2d953c7253ffad4ce5300aa49bbe27a86d
SHAHARIYER AHAMMED <shahariyer.as@gmail.com>
2023-01-05 20:31:44 +0600

    my data show

--- src/components/MyData/MyData.js
+++ src/components/MyData/MyData.js
@@ -4,8 +4,11 @@ import useStore from "../../hooks/useStore";
 const MyData = () => {
   const [selectors] = useStore();
   return (
-    <div>
-      <h2>DEta store loca{selectors.length}</h2>
+    <div className="mt-5">
+      {selectors.length < 1 && (
+        <div className="text-danger">No submit yet. Please submit</div>
+      )}
+      <h3 className="text-danger">Total submit {selectors.length}</h3>
       {(selectors.map = (m) => console.log(m))}
     </div>
   );
