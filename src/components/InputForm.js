7ba237b40d4e4b7f95226cb821e93eb8fded2c44
SHAHARIYER AHAMMED <shahariyer.as@gmail.com>
2023-01-05 09:22:40 +0600

    input form fixed

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,33 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const InputForm = () => {
-  const { control, handleSubmit, register } = useForm({
-    defaultValues: {
-      firstName: "",
-      select: {},
-    },
-  });
-  const onSubmit = (data) => console.log(data);
+  const [checkbox, setCheckbox] = useState(false);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName", { required: true, maxLength: 20 })} />
-      <div
-        name="select"
-        control={control}
-        render={({ field }) => (
-          <select
-            {...field}
-            options={[
-              { value: "chocolate", label: "Chocolate" },
-              { value: "strawberry", label: "Strawberry" },
-              { value: "vanilla", label: "Vanilla" },
-            ]}
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3 mx-auto w-25">
+          <label className="form-label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="name"
+            placeholder="Your Name"
+            className=" form-control"
+            {...register("name", {
+              required: {
+                value: true,
+                message: "Name is Required",
+              },
+            })}
           />
-        )}
-      />
-      <input type="submit" />
-    </form>
+          <label className="label">
+            {errors.name?.type === "required" && (
+              <span className="label-text-alt text-danger">
+                {errors.name?.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        <div className="mb-3 mx-auto w-25">
+          <label className="label">
+            <span className="label-text">Specialty</span>
+          </label>
+          <select {...register("specialty")} className=" form-control">
+            <option value="">on oafs</option>
+            {/* {services.map((service) => (
+              <option key={service._id} value={service.name}>
+                {service.name}
+              </option>
+            ))} */}
+          </select>
+        </div>
+        <div className="mb-3 mx-auto w-25">
+          <input
+            {...register("checkbox")}
+            type="checkbox"
+            value="accepts"
+            onChange={(e) => setCheckbox(e.target.checked)}
+          />
+          <label className="form-label">
+            <span className="label-text p-2"> Agree to terms</span>
+          </label>
+          <label className="label">
+            {errors.name?.type === "required" && (
+              <span className="label-text-alt text-danger">
+                {errors.checkbox?.message}
+              </span>
+            )}
+          </label>
+        </div>
+        <input
+          className="d-flex justify-content-center btn btn-primary mx-auto w-25"
+          type="submit"
+          value="Save"
+          disabled={!checkbox}
+        />
+      </form>
+    </div>
   );
 };
 
