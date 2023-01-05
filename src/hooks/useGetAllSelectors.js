367361f2564ec14c636d8f02cb09eddc5b25e672
SHAHARIYER AHAMMED <shahariyer.as@gmail.com>
2023-01-05 21:37:49 +0600

    fecth add

--- src/hooks/useGetAllSelectors.js
+++ src/hooks/useGetAllSelectors.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const useGetAllSelectors = () => {
   const [inputSelect, setInputSelect] = useState([]);
   useEffect(() => {
-    fetch("https://selectbox-react-server.vercel.app/")
+    fetch("https://selectbox-react-server.vercel.app/selectors")
       .then((res) => res.json())
       .then((data) => setInputSelect(data));
   }, []);
